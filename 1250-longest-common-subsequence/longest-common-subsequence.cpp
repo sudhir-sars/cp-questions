939d0737ea4aeed11a814c2d3c3225892c923dce
sudhir saraswat <130700267+sudhir-sars@users.noreply.github.com>
2026-02-14 17:54:24 +0530

    Time: 15 ms (92.17%) | Memory: 9 MB (96.96%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -6,24 +6,25 @@ public:
         int lenText1 = text1.length();
         int lenText2 = text2.length();
 
-        vector<vector<int>> lcsCache(lenText1+1,
-                                     vector<int>(lenText2+1, 0));
+        vector<int> prev(lenText2+1,0);
+        vector<int> curr(lenText2+1,0);
 
         for(int index1 = 1; index1 <= lenText1; ++index1) {
             for(int index2 = 1; index2 <= lenText2; ++index2) {
 
                 if(text1[index1-1] == text2[index2-1]) {
-                    lcsCache[index1][index2] =
-                        1 + lcsCache[index1-1][index2-1];
+                    curr[index2] =
+                        1 + prev[index2-1];
                 } 
                 else {
-                    lcsCache[index1][index2] =
-                        max(lcsCache[index1-1][index2],
-                            lcsCache[index1][index2-1]);
+                    curr[index2] =
+                        max(prev[index2],
+                            curr[index2-1]);
                 }
             }
+            prev=curr;
         }
 
-        return lcsCache[lenText1][lenText2];
+        return curr[lenText2];
     }
 };
