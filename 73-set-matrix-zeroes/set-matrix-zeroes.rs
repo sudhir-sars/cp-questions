b1fdfb9558f60a636cc710afe7146d299fe82973
sudhir saraswat <130700267+sudhir-sars@users.noreply.github.com>
2026-03-20 22:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 2.5 MB (28.57%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.rs
+++ 73-set-matrix-zeroes/set-matrix-zeroes.rs
@@ -1,47 +1,55 @@
 impl Solution {
     pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
-        let m=matrix.len();
-        let n=matrix[0].len();
+        if matrix.is_empty() || matrix[0].is_empty() {
+            return;
+        }
 
-        let mut first_row_zero=false;
-        let mut first_col_zero=false;
-        for i in 0..n{
-            if matrix[0][i]==0{
-                first_row_zero=true;
+        let mut first_row_zero = false;
+        let mut first_col_zero = false;
+
+        let num_rows = matrix.len();
+        let num_cols = matrix[0].len();
+
+        for j in 0..num_cols {
+            if matrix[0][j] == 0 {
+                first_row_zero = true;
                 break;
             }
         }
-        for i in 0..m{
-            if matrix[i][0]==0{
-                first_col_zero=true;
+
+        for i in 0..num_rows {
+            if matrix[i][0] == 0 {
+                first_col_zero = true;
                 break;
             }
         }
 
-        for i in 1..m{
-            for j in 1..n{
-                if matrix[i][j]==0{
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+        for i in 1..num_rows {
+            for j in 1..num_cols {
+                if matrix[i][j] == 0 {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for i in 1..m{
-            for j in 1..n{
-                if matrix[i][0]==0 || matrix[0][j]==0{
-                    matrix[i][j]=0;
+        for i in 1..num_rows {
+            for j in 1..num_cols {
+                if matrix[i][0] == 0 || matrix[0][j] == 0 {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        if first_row_zero{
-            for i in 0..n{
-                matrix[0][i]=0;
+
+        if first_row_zero {
+            for j in 0..num_cols {
+                matrix[0][j] = 0;
             }
         }
-         if first_col_zero{
-            for i in 0..m{
-                matrix[i][0]=0;
+
+        if first_col_zero {
+            for i in 0..num_rows {
+                matrix[i][0] = 0;
             }
         }
     }
